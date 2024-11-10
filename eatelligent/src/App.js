e53c016146a0693c83eab6aa4453fc791dc8e633
Robert <robertvuia06@gmail.com>
2024-11-10 16:03:48 +0200

    remove unnecessary files

--- eatelligent/src/App.js
+++ eatelligent/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <ul className="navbar">
+            <li><a href="/">Home</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/about">About Us</a></li>
+            <li><button>Sign Up</button></li>
+            <li><button>Sign In</button></li>
+          </ul>
+        </nav>
+        <h1>Eatelligent</h1>
       </header>
     </div>
   );
